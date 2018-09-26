1fb364ff8cdd7b3b7b863a973b0d4c4def357746
erha19 <faterrole@gmail.com>
2018-09-26 17:06:56 +0800

    update test case

--- tests/lib/rules/vue/valid-image-component.js
+++ tests/lib/rules/vue/valid-image-component.js
@@ -171,6 +171,20 @@ tester.run('valid-image-component', rule, {
         width: 120px;
       }
     </style>`
+    },
+    {
+      filename: 'test.vue',
+      code: `<template>
+        <div>
+        <image class="images-class" :src="col.itemImg"></image>
+        </div>
+      </template>
+      <style>
+      .images-class {
+        height: 20px;
+        width: 120px;
+      }
+    </style>`
     }
 
   ],
